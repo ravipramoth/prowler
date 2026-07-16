bc3d7452f4b87ea88909841c2fc117a9573bbf39
ravipramoth <40786130+ravipramoth@users.noreply.github.com>
2026-07-16 13:18:14 +0530

    Add files via upload

---
+++ all-dashboard/dist/server/__vite_rsc_assets_manifest.js
@@ -0,0 +1,47 @@
+export default {
+  "bootstrapScriptContent": "import(\"/assets/index-B3gez1uq.js\")",
+  "clientReferenceDeps": {
+    "6efdf509a785": {
+      "js": [
+        "/assets/page-B2cfiRlZ.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-CXnKph_e.js",
+        "/assets/index-B3gez1uq.js"
+      ],
+      "css": []
+    },
+    "d041429ec63f": {
+      "js": [
+        "/assets/index-B3gez1uq.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-CXnKph_e.js"
+      ],
+      "css": []
+    },
+    "e562e7309659": {
+      "js": [
+        "/assets/layout-segment-context-RZLybxaD.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/index-B3gez1uq.js",
+        "/assets/framework-CXnKph_e.js"
+      ],
+      "css": []
+    },
+    "8a1c26e77cc3": {
+      "js": [
+        "/assets/index-B3gez1uq.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-CXnKph_e.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index-BLnX1jZW.css"
+      ]
+    }
+  }
+}
