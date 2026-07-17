f396557a1c56213d1a933b072c50f9fa7638be9b
ravipramoth <40786130+ravipramoth@users.noreply.github.com>
2026-07-17 21:41:23 +0530

    Delete all-dashboard directory

--- all-dashboard/amplify/backend.ts
+++
@@ -1,8 +0,0 @@
-import { defineBackend } from "@aws-amplify/backend";
-import { auth } from "./auth/resource";
-import { storage } from "./storage/resource";
-
-defineBackend({
-  auth,
-  storage,
-});
